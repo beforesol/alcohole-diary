e746b1eb354da478d1f2a4f7d86e44fabde2c3e8
wonbincho <wjdqkseo@gmail.com>
2020-10-24 17:13:13 +0900

    layer setting

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Home.scss';
 import { Link } from 'react-router-dom';
@@ -11,10 +11,15 @@ interface IOwnProps {
 const Home: React.FC<IOwnProps> = ({
 
 }) => {
+  const [showLayer, setShowLayer] = useState(false);
   return (
     <div>
       <h1>Home</h1>
       <Link to='/detail/1'>go Detail!</Link>
+      <div><button>버튼</button></div>
+      {showLayer && (
+        <div className={cx('layer')}>레이어</div>
+      )}
     </div>
   );
 };
