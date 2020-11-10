e914016d9550c441724bab34aa4c2ba9bc80cfdc
전솔 <jeonsol@nhn.com>
2020-11-10 13:51:37 +0900

    svgr/webpack 설정

--- front-end/src/@types/index.d.ts
+++ front-end/src/@types/index.d.ts
@@ -132,8 +132,14 @@ declare module 'alcoholeDiary' {
     }
 }
 
+interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> { }
+
 declare module '*.scss';
 declare module '*.module.scss' {
     const classes: { [key: string]: string };
     export default classes;
+}
+declare module '*.svg' {
+    const value: SvgrComponent;
+    export default value;
 }
--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -4,6 +4,7 @@ import styles from './Header.scss';
 import { useLogin } from 'hooks/useLogin';
 import { Link } from 'react-router-dom';
 import { ROUTE_PATH } from '../routes';
+import Arrow from '@assets/img/svg/icon_logout.svg';
 
 const cx = classNames.bind(styles);
 
@@ -18,7 +19,9 @@ const Header: React.FC<IOwnProps> = ({
     <div className={cx('header')}>
       <Link
         to={ROUTE_PATH.LOGIN.path}
-        className={cx('login_btn')}>로그아웃</Link>
+        className={cx('logout_btn')}>
+        <Arrow className={cx('icon_logout')} />
+      </Link>
       <Link to={ROUTE_PATH.HOME.path}>Alcohole Diary</Link>
       <button
         type="button"
