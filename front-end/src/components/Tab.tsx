db9bbb5929bd95bc203509437f22a7f51da42433
wonbincho <wjdqkseo@gmail.com>
2020-11-29 18:36:12 +0900

    move statistics

--- front-end/src/components/Tab.tsx
+++ front-end/src/components/Tab.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Tab.scss';
+import { ROUTE_PATH } from '../routes';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -12,7 +14,10 @@ const Tab: React.FC<IOwnProps> = ({
   return (
     <div className={cx('tab')}>
       <button type="button" className={cx('tab_btn')}>달력</button>
-      <button type="button" className={cx('tab_btn')}>통계</button>
+      <Link 
+        to={ROUTE_PATH.STATISTICS.path}
+        type = 'button'
+        className={cx('tab_btn')}>통계</Link> 
       <button type="button" className={cx('tab_btn')}>???</button>
       <button type="button" className={cx('tab_btn')}>설정</button>
     </div>
