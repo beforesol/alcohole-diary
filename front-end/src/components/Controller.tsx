c9cb21274c68e1a1057812bc8871e1755c70c70a
wonbincho <wjdqkseo@gmail.com>
2020-12-11 00:42:04 +0900

    위 변경 가능

--- front-end/src/components/Controller.tsx
+++ front-end/src/components/Controller.tsx
@@ -10,6 +10,10 @@ interface IOwnProps {
 
 const Controller: React.FC<IOwnProps> = ({
 }) => {
+
+  const Unit = ['잔', 'ml', '병'];
+  const [selectedItem, setSeletedItem] = useState(0);
+
   const [showUnitLayer, setShowUnitLayer] = useState(false);
   const [AlcoholList, setAlcoholList] = useState([
     {
@@ -66,9 +70,25 @@ const Controller: React.FC<IOwnProps> = ({
     setAlcoholList(list)
   };
 
-  const handleClickUnit = () => {
+  const handleClickChangeUnit = (unit: string) => {
     setShowUnitLayer(false);
-  }
+    const id = selectedItem;
+    const list = AlcoholList.map(item => {
+      if(item.id === id){
+          return{
+            ...item,
+            unit 
+          } 
+      }
+      return item
+    })
+    setAlcoholList(list)
+  };
+
+  const handleClickUnit = (id: number) => {
+    setShowUnitLayer(true);
+    setSeletedItem(id);
+  };
 
   return (
     <div className={cx('controller')}>
@@ -79,17 +99,18 @@ const Controller: React.FC<IOwnProps> = ({
             <div className={cx('btn_area')}>
               <button onClick={() => handleClickPlus(Alcohol.id)} className={cx('btn', 'count_btn')}>+</button>
               <button onClick={() => handleClickMinus(Alcohol.id)} className={cx('btn', 'count_btn')}>-</button>
-              <button type="button" className={cx('btn', 'unit_btn')} onClick={() => setShowUnitLayer(true)}>단위</button>
+              <button type="button" className={cx('btn', 'unit_btn')} onClick={() => handleClickUnit(Alcohol.id)}>단위</button>
             </div>
           </li>
         )}
       </ul>
+
       {showUnitLayer && (
         <div className={cx('unit_layer')}>
           <div className={cx('layer_inner')}>
-            <button type="button" className={cx('layer_item')} onClick={handleClickUnit}>잔</button>
-            <button type="button" className={cx('layer_item')} onClick={handleClickUnit}>ml</button>
-            <button type="button" className={cx('layer_item')} onClick={handleClickUnit}>병</button>
+            <button type="button" className={cx('layer_item')} onClick={() => handleClickChangeUnit(Unit[0])}>잔</button>
+            <button type="button" className={cx('layer_item')} onClick={() => handleClickChangeUnit(Unit[1])}>ml</button>
+            <button type="button" className={cx('layer_item')} onClick={() => handleClickChangeUnit(Unit[2])}>병</button>
             <button type="button" className={cx('close_btn')} onClick={() => setShowUnitLayer(false)}>
               <Close className={cx('icon_close')} />
               <span className={cx('blind')}>닫기</span>
