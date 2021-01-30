1cc4c82d0b70531b96ec2716bd9a86cb862fd179
전솔 <jeonsol12@naver.com>
2021-01-30 11:59:20 +0900

    recodeReducer

--- front-end/src/reducers/RecodeReducer.ts
+++ front-end/src/reducers/RecodeReducer.ts
@@ -1,9 +1,20 @@
 import { PayloadAction, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { IRecode } from '@src/models/recode';
 
 type recodeState = {
+  recode: {
+    isLoaded: boolean;
+    isFailed: boolean;
+    data: IRecode[]
+  }
 }
 
 const recodeState: recodeState = {
+  recode: {
+    isLoaded: false,
+    isFailed: false,
+    data: [],
+  }
 };
 
 export const slice = createSlice({
